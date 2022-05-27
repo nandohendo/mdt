71d01c698dd64001a7acec59d28571100c067642
Nando <fernando.prayogo@dana.id>
2022-05-27 19:10:56 +0700

    initial commit

---
+++ mdt/mdt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mdt
+//
+//  Created by Fernando Prayogo on 27/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
