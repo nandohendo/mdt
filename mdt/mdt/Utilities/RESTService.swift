9fe80470498b8a350a0ba2223b47f634674c6366
Nando <fernando.prayogo@dana.id>
2022-05-28 23:32:53 +0700

    tidy up RESTService class a bit

--- mdt/mdt/Utilities/RESTService.swift
+++ mdt/mdt/Utilities/RESTService.swift
@@ -12,15 +12,18 @@ protocol RESTServiceable {
 }
 
 final class RESTService: RESTServiceable {
+	
+	enum HTTPMethod: String {
+		case get = "GET"
+		case post = "POST"
+	}
 
 	private let session = URLSession.shared
 	private let baseURL: String = "https://green-thumb-64168.uc.r.appspot.com"
 	
 	func makeLoginRequest(loginRequest: LoginRequest, completionHandler: @escaping ((Bool) -> Void)) {
-		var request = URLRequest(url: URL(string: "\(baseURL)/login")!)
-		request.httpMethod = "POST"
+		var request = getURLRequest(endPoint: "login", httpMethod: .post)
 		request.httpBody = try? JSONEncoder().encode(loginRequest)
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
 		let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
 			guard let data = data else {
@@ -38,6 +41,7 @@ final class RESTService: RESTServiceable {
 				KeychainHelper.cache(value: loginResponse.username, for: .username)
 				completionHandler(true)
 			} catch {
+				
 			}
 		})
 
@@ -45,19 +49,20 @@ final class RESTService: RESTServiceable {
 	}
 	
 	func makeGetBalanceRequest(completionHandler: @escaping ((Bool, BalanceResponse?) -> Void)) {
-		var request = URLRequest(url: URL(string: "\(baseURL)/balance")!)
-		request.httpMethod = "GET"
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.addValue((KeychainHelper.value(for: .token) ?? ""), forHTTPHeaderField: "Authorization")
+		let request = getURLRequest(endPoint: "balance", httpMethod: .get)
 
 		let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
 			guard let data = data else {
 				return
 			}
 			do {
-				
-				// should check for status from response too
 				let balanceResponse = try JSONDecoder().decode(BalanceResponse.self, from: data)
+				
+				if balanceResponse.status != "success" {
+					completionHandler(false, nil)
+					return
+				}
+				
 				completionHandler(true, balanceResponse)
 			} catch {
 				completionHandler(false, nil)
@@ -69,10 +74,7 @@ final class RESTService: RESTServiceable {
 	}
 	
 	func makeGetPayeeRequest(completionHandler: @escaping ((Bool, PayeeResponse?) -> Void)) {
-		var request = URLRequest(url: URL(string: "\(baseURL)/payees")!)
-		request.httpMethod = "GET"
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.addValue((KeychainHelper.value(for: .token) ?? ""), forHTTPHeaderField: "Authorization")
+		let request = getURLRequest(endPoint: "payees", httpMethod: .get)
 
 		let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
 			guard let data = data else {
@@ -93,19 +95,20 @@ final class RESTService: RESTServiceable {
 	}
 	
 	func makeGetHistoryRequest(completionHandler: @escaping ((Bool, TransferResponse?) -> Void)) {
-		var request = URLRequest(url: URL(string: "\(baseURL)/transactions")!)
-		request.httpMethod = "GET"
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.addValue((KeychainHelper.value(for: .token) ?? ""), forHTTPHeaderField: "Authorization")
+		let request = getURLRequest(endPoint: "transactions", httpMethod: .get)
 
 		let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
 			guard let data = data else {
 				return
 			}
 			do {
-				
-				// should check for status from response too
 				let historyResponse = try JSONDecoder().decode(TransferResponse.self, from: data)
+				
+				if historyResponse.status != "success" {
+					completionHandler(false, nil)
+					return
+				}
+				
 				completionHandler(true, historyResponse)
 			} catch {
 				completionHandler(false, nil)
@@ -114,6 +117,17 @@ final class RESTService: RESTServiceable {
 		})
 
 		task.resume()
+	}
+	
+	private func getURLRequest(endPoint: String, httpMethod: HTTPMethod) -> URLRequest {
+		var request = URLRequest(url: URL(string: "\(baseURL)/\(endPoint)")!)
+		request.httpMethod = httpMethod.rawValue
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		if endPoint != "register" && endPoint != "login" {
+			request.addValue((KeychainHelper.value(for: .token) ?? ""), forHTTPHeaderField: "Authorization")
+		}
+		return request
 	}
 	
 }
